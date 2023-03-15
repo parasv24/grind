4700b90e880fa803587ca78ed91e207c9e36e368
Paras Verma <90893097+parasv24@users.noreply.github.com>
2023-03-15 14:57:45 +0530

    easy recursive approach

---
+++ 2487-remove-nodes-from-linked-list/additional.py
@@ -0,0 +1,6 @@
+  def removeNodes(self, head):
+      if not head: return None
+      head.next = self.removeNodes(head.next)
+      if head.next and head.val < head.next.val:
+          return head.next
+      return head
