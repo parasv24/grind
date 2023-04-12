9237e37dfb9bb182ba2a2f2faf3fbb93417e442a
Paras Verma <90893097+parasv24@users.noreply.github.com>
2023-04-12 12:58:57 +0530

    Time: 1236 ms (5.13%), Space: 13.8 MB (91.57%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,4 +1,8 @@
 class Solution:
+    dp = {}
     def fib(self, n: int) -> int:
+        ans = self.dp.get(n, -1) 
+        if ans!= -1:
+            return ans
         return self.fib(n-1) + self.fib(n-2) if n > 1 else n
         
