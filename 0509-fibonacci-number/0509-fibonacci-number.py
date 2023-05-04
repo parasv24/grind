fb60134c58190ccd7b3c3165fc1d5eb6eb42424f
Paras Verma <90893097+parasv24@users.noreply.github.com>
2023-05-04 11:52:24 +0530

    Time: 15 ms (80.42%), Space: 13.3 MB (88.61%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,8 +1,37 @@
-class Solution:
-    dp = {}
-    def fib(self, n: int) -> int:
-        ans = self.dp.get(n, -1) 
-        if ans!= -1:
-            return ans
+class Solution(object):
+    def fibrec(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
         return self.fib(n-1) + self.fib(n-2) if n > 1 else n
+    def fib(self, n):
+        dp = [-1 for _ in range(100)]
+        def fibmem(n):
+            """
+            :type n: int
+            :rtype: int
+            """
+            if n < 2:
+                dp[n] = n
+                return dp[n]
+            if dp[n] != -1:
+                return dp[n]
+            else:
+                dp[n] = fibmem(n-1) + fibmem(n-2)
+                return dp[n]
+        def fibdp(n):
+            """
+            :type n: int
+            :rtype: int
+            """
+            dp[0] = 0
+            dp[1] = 1
+            for i in range(2,100):
+                dp[i] = dp[i-1] + dp[i-2]
+            return dp[n]
+        print(fibdp(n))
+        return fibmem(n)
+        
+
         
