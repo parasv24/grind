76e569920f582bbc28e696340a00f45fbca2f9fb
Paras Verma <90893097+parasv24@users.noreply.github.com>
2023-05-04 12:26:58 +0530

    Time: 269 ms (46.14%), Space: 31 MB (32.81%) - LeetHub

---
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def numIslands(self, grid):
+        """
+        :type grid: List[List[str]]
+        :rtype: int
+        """
+        n = len(grid)
+        m = len(grid[0])
+        vis = [[0 for _ in range(m)] for _ in range(n)]
+        delr = [ -1 , 0 , 0 , 1]
+        delc = [ 0, -1, 1, 0]
+        def dfs(r , c):
+            vis[r][c] = 1
+            for i in range(0, 4):
+                nr = r + delr[i]
+                nc = c + delc[i]
+                if 0 <= nr < n and 0 <= nc < m and vis[nr][nc] == 0 and grid[nr][nc] == '1':
+                    dfs(nr, nc)
+        count = 0
+        for i in range(0, n):
+            for j in range(0, m):
+                if vis[i][j] == 0 and grid[i][j] == '1':
+                    dfs(i, j)
+                    count += 1
+        return count
