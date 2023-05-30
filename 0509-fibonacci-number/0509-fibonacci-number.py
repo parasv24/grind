07041462cf10368827b68d282c018da2d94476eb
Paras Verma <90893097+parasv24@users.noreply.github.com>
2023-05-30 12:18:41 +0530

    Time: 41 ms (61.15%), Space: 16.3 MB (6.71%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,5 +1,17 @@
 class Solution:
     def fib(self, n: int) -> int:
+        # REcursive
         def f(n):
             return f(n-1) + f(n-2) if n > 1 else n
-        return f(n)
+        # return f(n)
+    
+        # Memo
+        dp = [-1 for _ in range(n+1)]
+        def m(n):
+            if n <=1:
+                return n
+            if dp[n]!= -1:
+                return dp[n]
+            dp[n] = m(n-1) + m(n-2)
+            return dp[n]
+        return m(n)
