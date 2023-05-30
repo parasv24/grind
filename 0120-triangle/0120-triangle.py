4353b4e8d8c962e0c0fe1ec8a4d22a56ff26cf88
Paras Verma <90893097+parasv24@users.noreply.github.com>
2023-05-30 15:37:56 +0530

    Time: 77 ms (51.55%), Space: 17.3 MB (33.51%) - LeetHub

--- 0120-triangle/0120-triangle.py
+++ 0120-triangle/0120-triangle.py
@@ -1,15 +1,12 @@
 class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
         n = len(triangle)
-        dp = [[-1]*n for _ in range(n)]
-        def mem(i,j):
-            if j > i:
-                return 100000000
-            if i == n-1:
-                return triangle[i][j]
-            if dp[i][j] != -1:
-                return dp[i][j]
-            
-            dp[i][j] = triangle[i][j] + min(mem(i+1,j), mem(i+1, j+1))
-            return dp[i][j]
-        return mem(0,0)
+        dp = [-1]*n
+        for i in range(n):
+            dp[i] = triangle[n-1][i]
+        
+        for i in range(n-2, -1, -1):
+            for j in range(0, i+1):
+                dp[j] = triangle[i][j] + min(dp[j], dp[j+1])
+        return dp[0]
+        
