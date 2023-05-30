b1dd3662593b469e51444e79a76be27d9902669a
Paras Verma <90893097+parasv24@users.noreply.github.com>
2023-05-30 12:39:36 +0530

    Time: 65 ms (62.10%), Space: 16.3 MB (52.08%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
@@ -1,19 +1,19 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        def f(n):
-            if n >= len(cost):
-                return 0
-            return cost[n] + min(f(n+1),f(n+2))
+        # def f(n):
+        #     if n >= len(cost):
+        #         return 0
+        #     return cost[n] + min(f(n+1),f(n+2))
         # return min(f(0), f(1))
         
         dp = [-1 for _ in range(len(cost)+1)]
-        def m(n):
-            if n >= len(cost):
-                return 0
-            if dp[n]!= -1:
-                return dp[n]
-            dp[n] = cost[n] + min(m(n+1),m(n+2))
-            return dp[n]
+        # def m(n):
+        #     if n >= len(cost):
+        #         return 0
+        #     if dp[n]!= -1:
+        #         return dp[n]
+        #     dp[n] = cost[n] + min(m(n+1),m(n+2))
+        #     return dp[n]
         # return min(m(0), m(1))
         
         # TABULATION
