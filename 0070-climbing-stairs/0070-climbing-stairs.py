880191603aacc7898a64877c499a54bc2fab4e00
Paras Verma <90893097+parasv24@users.noreply.github.com>
2023-05-30 12:14:42 +0530

    Time: 46 ms (31.59%), Space: 16.5 MB (9.97%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -14,8 +14,9 @@ class Solution:
             return dp[n]
         
         # Tabulation
-        # dp[0] = 1
-        # dp[1] = 1
-        # for i in range(2, n+1):
-        #     fp
-        return m(n)
+        dp[0] = 1
+        dp[1] = 1
+        for i in range(2, n+1):
+            dp[i] = dp[i-1] + dp[i-2]
+        
+        return dp[n]
