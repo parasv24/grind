90950c7b8365cd641933061dc0f667c23b5eb78f
Paras Verma <90893097+parasv24@users.noreply.github.com>
2023-05-30 12:20:00 +0530

    Time: 44 ms (55.30%), Space: 16.4 MB (6.71%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -14,4 +14,12 @@ class Solution:
                 return dp[n]
             dp[n] = m(n-1) + m(n-2)
             return dp[n]
-        return m(n)
+        # return m(n)
+        
+        if n <=1:
+            return n
+        dp[0] = 0
+        dp[1] = 1
+        for i in range(2, n+1):
+            dp[i] = dp[i-1] + dp[i-2]
+        return dp[n]
