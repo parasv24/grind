3d29678a6557c39df3dca38b094e81dae1ba50a4
Paras Verma <90893097+parasv24@users.noreply.github.com>
2024-08-03 16:06:49 +0530

    Time: 88 ms (21.00%) | Memory: 27.1 MB (42.97%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.py
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.py
@@ -7,27 +7,31 @@
 class Solution:
     def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
         # 1st method
-        len1, len2 = 0, 0
-        temp1, temp2 = headA, headB
-        while temp1:
-            len1 += 1
-            temp1 = temp1.next
+        # len1, len2 = 0, 0
+        # temp1, temp2 = headA, headB
+        # while temp1:
+        #     len1 += 1
+        #     temp1 = temp1.next
         
-        while temp2:
-            len2 += 1
-            temp2 = temp2.next
+        # while temp2:
+        #     len2 += 1
+        #     temp2 = temp2.next
         
-        def get_intersection(a,b,diff):
-            while diff > 0:
-                a = a.next
-                diff -= 1
-            while a:
-                if a == b:
-                    return a
-                a, b = a.next, b.next
-            return None
-        if len1 > len2:
-            return get_intersection(headA, headB, abs(len1 - len2))
-        else:
-            return get_intersection(headB, headA, abs(len1 - len2))
-        
+        # def get_intersection(a,b,diff):
+        #     while diff > 0:
+        #         a = a.next
+        #         diff -= 1
+        #     while a:
+        #         if a == b:
+        #             return a
+        #         a, b = a.next, b.next
+        #     return None
+        # if len1 > len2:
+        #     return get_intersection(headA, headB, abs(len1 - len2))
+        # else:
+        #     return get_intersection(headB, headA, abs(len1 - len2))
+        d1, d2 = headA, headB
+        while d1 != d2:
+            d1 = headB if d1 is None else d1.next
+            d2 = headA if d2 is None else d2.next
+        return d1
