a90ae0301259f15836af64f7dbbca59717a22df3
Paras Verma <90893097+parasv24@users.noreply.github.com>
2024-12-18 16:54:58 +0530

    Time: 23 ms (15.35%) | Memory: 18.1 MB (8.76%) - LeetSync

--- 39-combination-sum/combination-sum.py
+++ 39-combination-sum/combination-sum.py
@@ -4,12 +4,11 @@ class Solution:
         final_ans = []
         def get_candidates(target, cur_sum, i, cur_vec):
             if cur_sum == target:
-                if cur_vec not in final_ans:
-                    final_ans.append(cur_vec)
+                final_ans.append(cur_vec)
                 return
             if i >= len(candidates) or cur_sum > target:
                 return
-            get_candidates(target, cur_sum + candidates[i], i+1, cur_vec + [candidates[i]])
+            # get_candidates(target, cur_sum + candidates[i], i+1, cur_vec + [candidates[i]])
             get_candidates(target, cur_sum + candidates[i], i, cur_vec + [candidates[i]])
             get_candidates(target, cur_sum + 0, i+1, cur_vec)
             return
