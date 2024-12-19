2b38ed8fd1d5c2640260fad37a5106b4654d22fd
Paras Verma <90893097+parasv24@users.noreply.github.com>
2024-12-19 13:54:11 +0530

    Time: 87 ms (93.41%) | Memory: 52.8 MB (9.89%) - LeetSync

--- 3430-count-days-without-meetings/count-days-without-meetings.py
+++ 3430-count-days-without-meetings/count-days-without-meetings.py
@@ -3,13 +3,13 @@ class Solution:
         meetings.sort(key=lambda x: x[0])
         prev_x , prev_y = meetings[0]
         ans = meetings[0][0] - 1
-        print(ans)
+        #print(ans)
         for x,y in meetings:
             if prev_y >= x:
                 prev_x = x
                 prev_y = max(prev_y, y)
             else:
-                print(ans, x, prev_y)
+                #print(ans, x, prev_y)
                 ans += x - prev_y - 1
                 prev_x , prev_y  = x, y
         return ans + days - prev_y
