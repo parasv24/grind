1ed6f0e79d9994a8177c917d31c04dfa66b66995
Paras Verma <90893097+parasv24@users.noreply.github.com>
2024-12-30 08:50:58 +0530

    Time: 206 ms (38.29%) | Memory: 139.2 MB (34.34%) - LeetSync

---
+++ 2562-count-ways-to-build-good-strings/count-ways-to-build-good-strings.py
@@ -0,0 +1,8 @@
+class Solution:
+    def countGoodStrings(self, low: int, high: int, zero: int, one: int) -> int:
+        @cache
+        def helper(length):
+            return (helper(length + zero) + helper(length + one) + int(low <= length <= high))% 1000000007 if length <= high else 0
+        return helper(0) % 1000000007
+
+        
