9353ea841dc2692fbf98e21dec51cfe0c5d4c697
Paras Verma <90893097+parasv24@users.noreply.github.com>
2025-01-11 12:29:17 +0530

    Time: 162 ms (41.39%) | Memory: 18.7 MB (43.76%) - LeetSync

---
+++ 907-koko-eating-bananas/koko-eating-bananas.py
@@ -0,0 +1,22 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        def can_eat_bananas(num):
+            ans = 0
+            for el in piles:
+                ans += ((el - 1) // num ) +1
+            if ans <= h:
+                return True
+            return False
+        piles.sort()
+        lo = 1
+        hi = piles[-1]
+        ans = -1
+        while lo <= hi:
+            mid = (lo + hi) // 2
+            if can_eat_bananas(mid):
+                ans = mid
+                hi = mid - 1
+            else:
+                lo = mid + 1
+        return ans
+        
