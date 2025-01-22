eabfd7a97a3a782b25cd1f39548dce7d65faa349
Paras Verma <90893097+parasv24@users.noreply.github.com>
2025-01-22 07:48:05 +0530

    Time: 244 ms (17.98%) | Memory: 20.6 MB (42.69%) - LeetSync

---
+++ 542-01-matrix/01-matrix.py
@@ -0,0 +1,23 @@
+class Solution:
+    def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
+        n , m = len(mat), len(mat[0])
+        graph = [[-1 if mat[i][j]!=0 else mat[i][j] for j in range(m)] for i in range(n)]
+        delta = [[-1, 0], [1, 0], [0, 1], [0, -1]]
+        queue = []
+        for i in range(n):
+            for j in range(m):
+                if graph[i][j] == 0:
+                    queue.append([i, j])
+        lvl_value = 1
+        while queue:
+            len_level = len(queue)
+            for i in range(len_level):
+                i, j = queue.pop(0)
+                val = graph[i][j]
+                for x, y in delta:
+                    if 0 <= i + x <= n-1 and 0 <= j + y <= m-1 and graph[i+x][j+y]== -1:
+                        graph[i+x][j+y] = lvl_value
+                        queue.append([i+x, y+j])
+            lvl_value += 1
+        return graph
+        
