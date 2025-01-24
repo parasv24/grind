177eb171bd03ba5db264296e7d852a7688e021c7
Paras Verma <90893097+parasv24@users.noreply.github.com>
2025-01-24 22:43:24 +0530

    Time: 512 ms (0.00%) | Memory: 218 MB (0.00%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.rb
+++ 820-find-eventual-safe-states/find-eventual-safe-states.rb
@@ -1,29 +1,32 @@
 # @param {Integer[][]} graph
 # @return {Integer[]}
 def eventual_safe_nodes(graph)
-  n = graph.size
-  vis = Array.new(n, 0)
-  path_vis = Array.new(n, 0)
-  safe = Array.new(n, 0)
-  define_method(:dfs) do |s, vis, path_vis, safe, graph|
-    vis[s] = 1
-    path_vis[s] = 1
-    for el in graph[s]
-      if vis[el] == 0
-        if dfs(el, vis, path_vis, safe, graph)
-          return true
+    n = graph.size
+    vis = Array.new(n, 0)
+    path_vis = Array.new(n, 0)
+    safe = Array.new(n, 0)
+    def dfs(s, vis, path_vis, graph, safe)
+        vis[s] = 1
+        path_vis[s] = 1
+        puts(s)
+        print(graph[s])
+        (graph[s]).each do |el|
+        if vis[el] == 0
+            if dfs(el, vis, path_vis, graph, safe)
+            return true
+            end
+        elsif path_vis[el] == 1
+            return true
         end
-      elsif path_vis[el] == 1
-        return true
-      end
+        end
+        path_vis[s] = 0
+        safe[s] = 1
+        return false
     end
-    path_vis[s] = 0
-    safe[s] = 1
-    false
-  end
   (0...n).each do |i|
-    dfs(i, vis, path_vis, safe, graph) if vis[i] == 0
+    dfs(i, vis, path_vis, graph, safe) if vis[i] == 0
   end
-
-  safe.each_index.select { |i| safe[i] == 1 }
+   return (0...n).select do |i|
+    safe[i] == 1
+  end
 end
