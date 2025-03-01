14b0ed86e3f8ca4626bc71fdd85e7b57022cf210
Paras Verma <90893097+parasv24@users.noreply.github.com>
2025-03-01 16:36:28 +0530

    Time: 0 ms (100.00%) | Memory: 211.8 MB (62.32%) - LeetSync

--- 733-flood-fill/flood-fill.rb
+++ 733-flood-fill/flood-fill.rb
@@ -10,7 +10,7 @@ def flood_fill(image, sr, sc, color)
     delta.each do |x, y|
         nr, nc = sr + x, sc + y
         if nr >= 0 && nr < image.size && nc >= 0 && nc < image[0].size && image[nr][nc] == og_color && og_color != color
-            image = flood_fill(image, nr, nc, color)
+            flood_fill(image, nr, nc, color)
         end
     end
     image
