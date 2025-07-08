8b20814d508093358b92bfba3104686cfc551903
Paras Verma <90893097+parasv24@users.noreply.github.com>
2025-07-08 11:26:53 +0530

    Time: 64 ms (64.49%) | Memory: 18.5 MB (14.69%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.py
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.py
@@ -1,31 +1,23 @@
 class Solution:
     def longestOnes(self, nums: List[int], k: int) -> int:
-        i , j = 0, 0
-        mp = {}
-        max_l = 0
-        cur_l = 0
-        og_k = k
+        maxi, i , j, used = 0 , 0 , 0, 0
         while j < len(nums):
-            if nums[j] == 1:
-                cur_l += 1
-                max_l = max(max_l, cur_l)
-                #print(j, nums[j], max_l, cur_l, k)
+            if nums[j] == 1 or used < k:
+                if nums[j] == 0 and used < k:
+                    used += 1
+                j += 1
+            elif k > 0:
+                maxi = max(maxi, j - i)
+                while i < j and used >= k:
+                    if nums[i] == 0:
+                        used -= 1
+                    i += 1
             else:
-                if k > 0:
-                    k -= 1
-                    cur_l += 1
-                    max_l = max(max_l, cur_l)
-                else:
-                    while cur_l > 0 and k <= 0:
-                        if nums[i] == 0:
-                            k += 1
-                            k = min(k, og_k)
-                        cur_l -= 1
-                        i += 1
-                    if k > 0:
-                        cur_l += 1
-                        k -= 1
-                        max_l = max(max_l, cur_l)
-                #print(j, nums[j], max_l, cur_l, k)
-            j += 1
-        return max_l
+                maxi = max(maxi, j - i)
+                i = j + 1
+                j += 1
+        maxi = max(maxi, j - i)
+        return maxi 
+
+
+                    
