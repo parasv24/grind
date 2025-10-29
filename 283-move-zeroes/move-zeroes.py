f63447ebf3eac51c5bead6f4c75eeb990a14693e
Paras Verma <90893097+parasv24@users.noreply.github.com>
2025-10-29 19:32:54 +0530

    Time: 3 ms (80.14%) | Memory: 18.7 MB (95.19%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -6,7 +6,5 @@ class Solution:
         idx = 0
         for i in range(len(nums)):
             if nums[i] != 0:
-                nums[idx] = nums[i]
+                nums[idx], nums[i] = nums[i], nums[idx]
                 idx += 1
-        for i in range(idx, len(nums)):
-            nums[i] = 0
