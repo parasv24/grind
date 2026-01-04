dbfc9c61d8f02be7eba9f08cbb7fa16c8a611b61
Paras Verma <90893097+parasv24@users.noreply.github.com>
2026-01-04 23:17:08 +0530

    Time: 0 ms (100.00%) | Memory: 17.3 MB (90.58%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.py
+++ 118-pascals-triangle/pascals-triangle.py
@@ -12,7 +12,20 @@ class Solution:
                 intut.append(last_arr[i] + last_arr[ i - 1])
             intut.append(1)
             return small_ans + [intut]
-        return rec(numRows)
+        # return rec(numRows)
+        ans = [[1]]
+        prev = [1]
+        numRows-=1
+        while numRows:
+            temp = [1]
+            for i in range(1, len(prev)):
+                temp.append(prev[i] + prev[i-1])
+            temp.append(1)
+            ans.append(temp)
+            prev = temp
+            numRows -= 1
+        return ans
+
 
             
         
