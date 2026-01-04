30da36884cf616e1271c77215ee57abe44200e83
Paras Verma <90893097+parasv24@users.noreply.github.com>
2026-01-04 23:12:56 +0530

    Time: 0 ms (100.00%) | Memory: 17.4 MB (83.21%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.py
+++ 118-pascals-triangle/pascals-triangle.py
@@ -1,22 +1,21 @@
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
-        if numRows == 1:
-            return [[1]]
-        ans = [[1], [1,1]]
-        numRows -= 2
-        prev = [1,1]
-        while numRows > 0:
-            temp = [1]
-            sm = prev[0]
-            for i in range(1,len(prev)):
-                sm += prev[i]
-                temp.append(sm)
-                sm -= prev[i-1]
-            temp.append(1)
-            ans.append(temp)
-            prev = temp
-            numRows -= 1
-        return ans
+        def rec(n):
+            if n == 1:
+                return [[1]]
+            if n == 2:
+                return [[1], [1,1]]
+            small_ans = rec(n-1)
+            last_arr = small_ans[-1]
+            intut = [1]
+            for i in range(1, len(last_arr)):
+                intut.append(last_arr[i] + last_arr[ i - 1])
+            intut.append(1)
+            return small_ans + [intut]
+        return rec(numRows)
+
+            
+        
 
 
         
