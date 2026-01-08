d200a2c89da25391aa9861430759f31a316466ba
Paras Verma <90893097+parasv24@users.noreply.github.com>
2026-01-08 17:23:24 +0530

    Time: 3 ms (7.56%) | Memory: 19.6 MB (7.33%) - LeetSync

--- 50-powx-n/powx-n.py
+++ 50-powx-n/powx-n.py
@@ -4,14 +4,16 @@ class Solution:
             return x
         if x == -1.0:
             return 1.0 if n % 2 == 0 else -1.0
-        if n < -1000000:
-            return 0
+        def is_zero(x, eps=1e-6):
+            return abs(x) < eps
         mp = {}
         power = abs(n)
         ans = 1
         while power > 0:
             if power & 1 == 1:
                 ans *= x
+                if is_zero(ans):
+                    return ans
             x*= x
             power //=2
         return ans if n > 0 else 1/ans
