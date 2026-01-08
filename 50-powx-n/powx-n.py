10807816bbe5382d649fc01703d68b88c5a138d4
Paras Verma <90893097+parasv24@users.noreply.github.com>
2026-01-08 17:00:06 +0530

    Time: 0 ms (100.00%) | Memory: 19.9 MB (7.33%) - LeetSync

--- 50-powx-n/powx-n.py
+++ 50-powx-n/powx-n.py
@@ -6,5 +6,13 @@ class Solution:
             return 1.0 if n % 2 == 0 else -1.0
         if n < -1000000:
             return 0
-        return pow(x, n)
+        @cache
+        def power(x, n):
+            if n == 0:
+                return 1
+            if n & 1 == 0:
+                return power(x, n//2) * power(x, n//2)
+            else:
+                return x * power(x, n//2) * power(x, n//2)
+        return power(x, abs(n)) if n > 0 else 1/power(x,abs(n))
         
