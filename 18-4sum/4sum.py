afa76a8996407a984f9757fb90b4f48086c174a5
Paras Verma <90893097+parasv24@users.noreply.github.com>
2026-01-09 22:50:09 +0530

    Time: 323 ms (77.85%) | Memory: 19.6 MB (5.21%) - LeetSync

--- 18-4sum/4sum.py
+++ 18-4sum/4sum.py
@@ -1,38 +1,34 @@
 class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
-        nums.sort()
         n = len(nums)
-        res = []
+        ans = []
+        nums.sort()
         for i in range(n-3):
-            sm = nums[i]
-                
-            if i > 0 and nums[i] == nums[i-1]:
+            if i!= 0 and nums[i] == nums[i-1]:
                 continue
-            
+            print("i", nums[i])
             for j in range(i+1, n-2):
-                sm = nums[i] + nums[j]
-
-                if j > i+1 and nums[j] == nums[j - 1]:
+                if j!= i+1 and nums[j] == nums[j-1]:
                     continue
-                
                 k = j + 1
                 l = n - 1
+                print("j", nums[j])
+                total = target - nums[i] - nums[j]
                 while k < l:
-                    sm = nums[i] + nums[j]+ nums[k] + nums[l]
-                    #print(i, j ,k ,l, sm)
-                    if sm > target:
+                    cur = nums[k] + nums[l]
+                    if cur > total:
                         l -= 1
-                    elif sm < target:
-                        k += 1
+                    elif cur < total:
+                        k +=1
                     else:
-                        res.append([nums[i], nums[j], nums[k], nums[l]])
-                        while k < l:
-                            l -= 1
-                            if nums[l] != nums[l+1]:
-                                break
-                        
-                        while k < l:
+                        ans.append([nums[i], nums[j], nums[k], nums[l]])
+                        k += 1
+                        l -= 1
+                        while k < l and nums[k] == nums[k-1]:
                             k += 1
-                            if nums[k] != nums[k-1]:
-                                break
-        return res
+                        while k < l and nums[l] == nums[l+1]:
+                            l -=1
+        return ans
+
+
+        
