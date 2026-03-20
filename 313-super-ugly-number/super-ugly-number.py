b093b474e08dc23e88a14bde40327e604544e3a5
Paras Verma <90893097+parasv24@users.noreply.github.com>
2026-03-20 20:36:15 +0530

    Time: 1633 ms (25.96%) | Memory: 23 MB (71.27%) - LeetSync

--- 313-super-ugly-number/super-ugly-number.py
+++ 313-super-ugly-number/super-ugly-number.py
@@ -2,20 +2,16 @@ class Solution:
     def nthSuperUglyNumber(self, n: int, primes: List[int]) -> int:
         arr = [1]
         n -= 1
-        idxs = {}
         mp = {}
         for el in primes:
-            mp[el] = []
-            idxs[el] = 0
+            mp[el] = 0
         while n > 0:
             mini = 10000000000
             for el in primes:
-                mp[el].append(arr[-1] * el)
+                mini = min(mini, arr[mp[el]] * el)
             for el in primes:
-                mini = min(mini, mp[el][idxs[el]])
-            for el in primes:
-                if mp[el][idxs[el]] == mini:
-                    idxs[el] += 1
+                if arr[mp[el]] * el == mini:
+                    mp[el] += 1
             arr.append(mini)
             n -= 1
         return arr[-1]
