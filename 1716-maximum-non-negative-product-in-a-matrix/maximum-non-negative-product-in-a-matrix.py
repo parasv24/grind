131f512407591ffa84999b9e6858cb8481a5e244
Paras Verma <90893097+parasv24@users.noreply.github.com>
2026-03-23 15:30:12 +0530

    Time: 4 ms (62.28%) | Memory: 19.3 MB (88.60%) - LeetSync

--- 1716-maximum-non-negative-product-in-a-matrix/maximum-non-negative-product-in-a-matrix.py
+++ 1716-maximum-non-negative-product-in-a-matrix/maximum-non-negative-product-in-a-matrix.py
@@ -1,38 +1,45 @@
 class Solution:
     def maxProductPath(self, grid: List[List[int]]) -> int:
         n , m = len(grid) , len(grid[0])
-        dp = [[grid[i][j] for j in range(m)] for i in range(n)]
-        for i in range(1, n):
-            for j in range(1, m):
-                ans1 = grid[i][j] * dp[i-1][j]
-                ans2 = grid[i][j] * dp[i][j-1]
-                dp[i][j] = max(ans1, ans2)
+        dp = [[[grid[i][j], grid[i][j]] for j in range(m)] for i in range(n)]
+        for i in range(0, n):
+            for j in range(0, m):
+                if i > 0 or j > 0:
+                    arr = []
+                    for ni, nj in [(i-1, j), (i, j-1)]:
+                        if ni >= 0 and nj >= 0:
+                            arr.append(grid[i][j] * dp[ni][nj][0])
+                            arr.append(grid[i][j] * dp[ni][nj][1])
+                    dp[i][j] = [max(arr), min(arr)]
+        # print(dp)
+        ans  = dp[n-1][m-1][0]
+        return ans % 1000000007 if ans >= 0 else -1 
         # print(dp)
         # return dp[n-1][m-1] if dp[n-1][m-1] >= 0 else -1
-        @cache
-        def rec(i, j):
-            if i >= n or j >= m:
-                return None
+        # @cache
+        # def rec(i, j):
+        #     if i >= n or j >= m:
+        #         return None
 
-            if i == n-1 and j == m-1:
-                return (grid[i][j], grid[i][j])
+        #     if i == n-1 and j == m-1:
+        #         return (grid[i][j], grid[i][j])
 
-            res = []
+        #     res = []
 
-            for ni, nj in [(i+1, j), (i, j+1)]:
-                child = rec(ni, nj)
-                if child is None:
-                    continue
+        #     for ni, nj in [(i+1, j), (i, j+1)]:
+        #         child = rec(ni, nj)
+        #         if child is None:
+        #             continue
 
-                cmax, cmin = child
-                res.extend([
-                    grid[i][j] * cmax,
-                    grid[i][j] * cmin
-                ])
-            if not res:
-                return None
+        #         cmax, cmin = child
+        #         res.extend([
+        #             grid[i][j] * cmax,
+        #             grid[i][j] * cmin
+        #         ])
+        #     if not res:
+        #         return None
 
-            return (max(res), min(res))
-        ans, _ = rec(0,0)
-        return ans % 1000000007 if ans >= 0 else -1
+        #     return (max(res), min(res))
+        # ans, _ = rec(0,0)
+        # return ans % 1000000007 if ans >= 0 else -1
             
