69015253308a02db5218403d3ed657a8acf2aceb
Paras Verma <90893097+parasv24@users.noreply.github.com>
2026-03-28 09:13:47 +0530

    Time: 47 ms (39.59%) | Memory: 34.3 MB (27.26%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.py
+++ 131-palindrome-partitioning/palindrome-partitioning.py
@@ -1,16 +1,16 @@
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
         ans = []
-        def rec(i, cur, arr):
+        def rec(i, prev, arr):
             if i == len(s):
-                if cur == "":
+                if prev == len(s) - 1:
                     ans.append(arr)
                 return
-            cur += s[i]
+            cur = s[prev+1: i+1]
             if cur == cur[::-1]:
-                rec(i+1, "", arr + [cur])
-            rec(i+1, cur, arr)
-        rec(0, "", [])
+                rec(i+1, i, arr + [cur])
+            rec(i+1, prev, arr)
+        rec(0, -1, [])
         return ans
         
             
