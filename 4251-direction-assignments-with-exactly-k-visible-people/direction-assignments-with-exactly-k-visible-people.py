7e8cd1ae6a43bf006a30877b4b4cad5c1498b71e
Paras Verma <90893097+parasv24@users.noreply.github.com>
2026-03-29 14:49:52 +0530

    Time: 692 ms (100.00%) | Memory: 21.2 MB (100.00%) - LeetSync

--- 4251-direction-assignments-with-exactly-k-visible-people/direction-assignments-with-exactly-k-visible-people.py
+++ 4251-direction-assignments-with-exactly-k-visible-people/direction-assignments-with-exactly-k-visible-people.py
@@ -1,11 +1,4 @@
 class Solution:
     def countVisiblePeople(self, n: int, pos: int, k: int) -> int:
-        facts = [1]
-        prev = 1
-        MOD = 1000000007
-        for i in range(1, 100000):
-            prev = (prev * i) % MOD
-            facts.append(prev)
-        divis = pow((facts[k] * facts[n-1-k]), MOD - 2, MOD)
-        return (facts[n-1] * 2 * divis) % MOD
+        return (comb(n-1,k) * 2) % 1000000007
         
