800e307a281f0eabe8efb031484298e9a88f6d09
Paras Verma <90893097+parasv24@users.noreply.github.com>
2026-03-30 16:27:13 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (91.81%) - LeetSync

--- 46-permutations/permutations.py
+++ 46-permutations/permutations.py
@@ -1,14 +1,18 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        def recur(arr):
-            ans=[]
-            if len(arr)==1:
-                return [arr]
-            for i in range(0,len(arr)):
-                newarr=arr[:i] + arr[i+1:]
-                smallans=recur(newarr)
-                for j in smallans:
-                    ans.append([arr[i]]+j)
+        def rec(i):
+            if i == len(nums) - 1:
+                return[[nums[-1]]]
+            small_ans = rec(i+1)
+            ans = []
+            for arr in small_ans:
+                for idx in range(len(arr)+1):
+                    prev = arr[0: idx] if idx > 0 else []
+                    suf = arr[idx: ] if idx < len(arr) else []
+                    ans.append([*prev, nums[i], *suf])
             return ans
-        return recur(nums)
+        return rec(0)
+                    
+
+
         
