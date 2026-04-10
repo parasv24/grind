3fec2b4c8ba330f4dcc701cde19870623303bbfa
Paras Verma <90893097+parasv24@users.noreply.github.com>
2026-04-10 14:39:36 +0530

    Time: 3 ms (8.64%) | Memory: 20.3 MB (97.23%) - LeetSync

---
+++ 792-binary-search/binary-search.py
@@ -0,0 +1,12 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        low  , hi = 0, len(nums) - 1
+        while low <= hi:
+            mid = (low + hi) // 2
+            if nums[mid] == target:
+                return mid
+            if nums[mid] > target:
+                hi = mid - 1
+            else:
+                low = mid + 1
+        return -1
