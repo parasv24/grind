56d48400332c00f7267a37849402e9911d8a428f
Paras Verma <90893097+parasv24@users.noreply.github.com>
2026-04-11 13:49:31 +0530

    Time: 3 ms (69.59%) | Memory: 19.3 MB (86.42%) - LeetSync

--- 410-split-array-largest-sum/split-array-largest-sum.py
+++ 410-split-array-largest-sum/split-array-largest-sum.py
@@ -1,23 +1,24 @@
 class Solution:
     def splitArray(self, nums: List[int], k: int) -> int:
-        def can_split(num):
-            ans = 1
+        l, r = max(nums), sum(nums)
+        def is_poss(min_sum):
             cur = 0
+            total_sub = 0
             for el in nums:
-                if cur + el > num:
-                    cur = el
-                    ans += 1
-                else:
+                if cur + el <= min_sum:
                     cur += el
-            return ans <= k
-        lo, hi = max(nums), sum(nums)
-        ans = -1
-        while lo <= hi:
-            mid = (lo + hi) // 2
-            if can_split(mid):
-                ans = mid
-                hi = mid - 1
-            else:
-                lo = mid + 1
-        return ans
+                else:
+                    cur = el
+                    total_sub += 1
+            if cur > 0:
+                total_sub += 1
+            return total_sub <= k
         
+        while l <= r:
+            mid = (l + r) // 2
+            # print(l, r, mid , is_poss(mid))
+            if is_poss(mid):
+                r = mid - 1
+            else:
+                l = mid + 1
+        return l
