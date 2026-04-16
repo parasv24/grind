85dd43e5ede4628ccd151bde93e264ccd0fd9836
Paras Verma <90893097+parasv24@users.noreply.github.com>
2026-04-16 18:37:07 +0530

    Time: 3 ms (36.28%) | Memory: 19.3 MB (53.36%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.py
+++ 8-string-to-integer-atoi/string-to-integer-atoi.py
@@ -1,31 +1,22 @@
 class Solution:
     def myAtoi(self, s: str) -> int:
+        i = 0
+        pos = 1
+        while i < len(s) and s[i] == " ":
+            i += 1
+        if i < len(s) and s[i] == "-":
+            pos = -1
+            i += 1
+        elif i < len(s) and s[i] == "+":
+            i += 1
         ans = 0
-        int_read = False
-        sign_read = 1
-        valid_values = [chr(i) for i in range(ord('0'), ord('9') + 1)]
-        valid_values += [" ", "+", "-"]
-        final_ans = 0
-        for i in range(len(s)):
-            if s[i] not in valid_values:
-                final_ans =  ans * sign_read
-                break
-            else:
-                if ord('0') <= ord(s[i]) <= ord('9'):
-                    int_read = True
-                    ans = ans * 10 + (ord(s[i]) - ord('0'))
-                else:
-                    if int_read:
-                        final_ans = ans * sign_read
-                        break
-                    if s[i] == "-":
-                        sign_read = -1
-                    if s[i] in ["+", "-"]:
-                        valid_values = [chr(i) for i in range(ord('0'), ord('9') + 1)]
-        final_ans = ans * sign_read
-        if final_ans < -2147483648:
+        while i < len(s) and "0" <= s[i] <= "9":
+            ans = ans * 10 + (ord(s[i]) - 48)
+            i += 1
+        ans = ans * pos
+
+        if ans  < -2147483648:
             return -2147483648
-        if final_ans >= 2147483647:
+        if ans > 2147483647:
             return 2147483647
-        return final_ans
-        
+        return ans
