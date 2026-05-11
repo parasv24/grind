cd03b279f3ad262add65b1a486632f08b45b0d40
Paras Verma <90893097+parasv24@users.noreply.github.com>
2026-05-11 13:39:51 +0530

    Time: 1075 ms (72.39%) | Memory: 74.2 MB (71.78%) - LeetSync

---
+++ 4095-number-of-balanced-integers-in-a-range/number-of-balanced-integers-in-a-range.py
@@ -0,0 +1,30 @@
+class Solution:
+    def countBalanced(self, low: int, high: int) -> int:
+        def count(s):
+            s = str(s)
+            @cache
+            def rec(i, limit_on,started, diff):
+                if i == len(s):
+                    return diff == 0
+
+                limit = int(s[i]) if limit_on else 9
+                ans = 0
+                for j in range(limit+1):
+                    if not started and j == 0:
+                        ans += rec(i+1, limit_on and j == limit, started, diff)
+                    else:
+                        diff_new = diff + j if i % 2 != 0 else diff - j
+                        ans += rec(i+1, limit_on and j == limit, True, diff_new)
+                return ans
+            return rec(0, True, False, 0)
+        odd, even = 0,0
+        for idx, i in enumerate(str(low)):
+            if idx % 2 == 0:
+                even = even + int(i)
+            else:
+                odd = odd + int(i)
+        return count(high) - count(low) + int(odd == even)
+
+
+
+        
