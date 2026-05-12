684181688afae8343a97229dd90d2fd4f1e704e0
Paras Verma <90893097+parasv24@users.noreply.github.com>
2026-05-12 22:00:53 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (78.10%) - LeetSync

--- 1342-queens-that-can-attack-the-king/queens-that-can-attack-the-king.py
+++ 1342-queens-that-can-attack-the-king/queens-that-can-attack-the-king.py
@@ -1,11 +1,14 @@
 class Solution:
     def queensAttacktheKing(self, queens: List[List[int]], king: List[int]) -> List[List[int]]:
         ans = []
+        mp = [[0 for _ in range(8)] for _ in range(8)]
+        for x,y in queens:
+            mp[x][y] = 1
         delta = [[-1, 0], [1, 0], [0, 1], [0, -1], [-1, -1], [1, 1], [-1, 1], [1, -1]]
         for dx, dy in delta:
             kx, ky = king
             while 0 <= kx < 8 and 0 <= ky < 8:
-                if [kx, ky] in queens:
+                if mp[kx][ky] == 1:
                     ans.append([kx, ky])
                     break
                 kx += dx
