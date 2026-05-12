5e80b911a43e904b2ac95ad46c20b3dd88abd5ae
Paras Verma <90893097+parasv24@users.noreply.github.com>
2026-05-12 09:34:33 +0530

    Time: 43 ms (95.05%) | Memory: 55.5 MB (60.40%) - LeetSync

--- 1784-minimum-initial-energy-to-finish-tasks/minimum-initial-energy-to-finish-tasks.py
+++ 1784-minimum-initial-energy-to-finish-tasks/minimum-initial-energy-to-finish-tasks.py
@@ -1,20 +1,27 @@
 class Solution:
     def minimumEffort(self, tasks: List[List[int]]) -> int:
-        tasks.sort(key= lambda x: (-(x[1]-x[0]), -x[0]))
+        # tasks.sort(key= lambda x: (-(x[1]-x[0]), -x[0]))
 
-        def is_poss(val):
-            for actual, minimum in tasks:
-                if minimum > val:
-                    return False
-                val -= actual
-            return val >= 0
+        # def is_poss(val):
+        #     for actual, minimum in tasks:
+        #         if minimum > val:
+        #             return False
+        #         val -= actual
+        #     return val >= 0
 
-        l, r = 0, 10 ** 9
-        while l <= r:
-            mid = (l + r) // 2
-            if is_poss(mid):
-                r = mid - 1
-            else:
-                l = mid + 1
-        return l
+        # l, r = 0, 10 ** 9
+        # while l <= r:
+        #     mid = (l + r) // 2
+        #     if is_poss(mid):
+        #         r = mid - 1
+        #     else:
+        #         l = mid + 1
+        # return l
+        tasks.sort(key = lambda x: x[1] - x[0])
+        ans = 0
+        for task in tasks:
+            ans = max(ans + task[0], task[1])
+        return ans
+
+
         
