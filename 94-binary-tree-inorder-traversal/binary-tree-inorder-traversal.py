97c12a9e5288186db8f45e2fe71fb057d35182b8
Paras Verma <90893097+parasv24@users.noreply.github.com>
2026-05-20 15:47:45 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (66.47%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.py
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.py
@@ -22,16 +22,39 @@ class Solution:
         #         inorder.append(temp.val)
         #         temp = temp.right
 
-        stck = []
-        temp = root
+        # stck = []
+        # temp = root
+        # inorder = []
+        # while stck or temp:
+        #     while temp:
+        #         stck.append(temp)
+        #         temp = temp.left
+        #     if stck:
+        #         node = stck.pop()
+        #         inorder.append(node.val)
+        #         temp = node.right
+        # return inorder
+
+        pre = []
         inorder = []
-        while stck or temp:
-            while temp:
-                stck.append(temp)
-                temp = temp.left
-            if stck:
-                node = stck.pop()
+        post = []
+
+        stck = []
+        if root:
+            stck.append([root, 1])
+        while stck:
+            node, val = stck.pop()
+            if val == 1:
+                pre.append(node.val)
+                stck.append([node, 2])
+                if node.left:
+                    stck.append([node.left, 1])
+            elif val == 2:
                 inorder.append(node.val)
-                temp = node.right
+                stck.append([node, 3])
+                if node.right:
+                    stck.append([node.right, 1])
+            else:
+                post.append(node.val)
         return inorder
             
