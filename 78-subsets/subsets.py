3db71c4a1ccb08a8832057954d8e7922cd8e4204
Paras Verma <90893097+parasv24@users.noreply.github.com>
2026-05-21 11:26:09 +0530

    Time: 3 ms (7.54%) | Memory: 19.8 MB (5.68%) - LeetSync

--- 78-subsets/subsets.py
+++ 78-subsets/subsets.py
@@ -1,14 +1,34 @@
 class Solution:
-    def subsets(self, nums: List[int]) -> List[List[int]]:
+    def subsets(self, nums):
+        class Node:
+            def __init__(self):
+                self.children = {}
+
+        root = Node()
+
+        def build(index, curr):
+            if index == len(nums):
+                return
+
+            for i in range(index, len(nums)):
+                val = nums[i]
+
+                if val not in curr.children:
+                    curr.children[val] = Node()
+
+                build(i + 1, curr.children[val])
+
+        build(0, root)
+
         ans = []
-        def rec(i):
-            if i == len(nums):
-                return [[]]
-            small_ans = rec(i+1)
-            new_ans = []
-            for el in small_ans:
-                new_ans.append(el)
-                new_ans.append(el + [nums[i]])
-            return new_ans
-        return rec(0)
-        
+
+        def dfs(node, path):
+            ans.append(path[:])
+
+            for val, child in node.children.items():
+                path.append(val)
+                dfs(child, path)
+                path.pop()
+
+        dfs(root, [])
+        return ans
