5e3a3f1327bea5e5c38c575f17eab22319d78609
Paras Verma <90893097+parasv24@users.noreply.github.com>
2026-05-21 07:52:03 +0530

    Time: 755 ms (5.07%) | Memory: 40.5 MB (5.86%) - LeetSync

--- 3329-find-the-length-of-the-longest-common-prefix/find-the-length-of-the-longest-common-prefix.py
+++ 3329-find-the-length-of-the-longest-common-prefix/find-the-length-of-the-longest-common-prefix.py
@@ -1,18 +1,41 @@
 class Solution:
     def longestCommonPrefix(self, arr1: List[int], arr2: List[int]) -> int:
-        mp = {}
-        for el in arr1:
-            s = ""
-            for ch in str(el):
-                s += ch
-                mp[s] = s
+        class TrieNode:
+            def __init__(self, is_end=False):
+                self.children = [0] * 10
+                self.is_end = is_end
+
+        root = TrieNode()
+
+        def get_digits(num):
+            ans = []
+            while num:
+                ans.append(num % 10)
+                num = num // 10
+            return ans
+
+        for num in arr1:
+            curr = root
+            digits = get_digits(num)
+            for i in range(len(digits) - 1, -1, -1):
+                if curr.children[digits[i]] == 0:
+                    curr.children[digits[i]] = TrieNode()
+                curr = curr.children[digits[i]]
+            curr.is_end = True
         ans = 0
-        for el in arr2:
-            s = ""
-            for ch in str(el):
-                s += ch
-                word = mp.get(s, "")
-                ans = max(ans, len(word))
+        for num in arr2:
+            curr = root
+            digits = get_digits(num)
+            print(digits)
+            length = 0
+            for i in range(len(digits) - 1, -1, -1):
+                if curr.children[digits[i]] != 0:
+                    length += 1
+                    curr = curr.children[digits[i]]
+                else:
+                    break
+            ans = max(ans, length)
         return ans
+
 
         
