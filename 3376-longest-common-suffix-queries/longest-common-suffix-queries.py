8c69b683eb66a426563f6043fbc9d5511159ad58
Paras Verma <90893097+parasv24@users.noreply.github.com>
2026-05-28 09:56:44 +0530

    Time: 1082 ms (77.99%) | Memory: 165 MB (77.36%) - LeetSync

---
+++ 3376-longest-common-suffix-queries/longest-common-suffix-queries.py
@@ -0,0 +1,31 @@
+class Solution:
+    def stringIndices(self, wordsContainer: List[str], wordsQuery: List[str]) -> List[int]:
+        tup = [[wordsContainer[i], i] for i in range(len(wordsContainer))]
+        tup.sort(key= lambda x: len(x[0]))
+        
+        class Node:
+            def __init__(self, val):
+                self.val = val
+                self.children = {}
+        
+        root = Node(tup[0][1])
+        for word, idx in tup:
+            curr = root
+            for i in range(len(word)-1, -1, -1):
+                ch = word[i]
+                if ch not in curr.children:
+                    curr.children[ch] = Node(idx)
+                curr = curr.children[ch]
+        ans = []
+        for word in wordsQuery:
+            curr = root
+            for i in range(len(word)-1, -1, -1):
+                ch = word[i]
+                if ch not in curr.children:
+                    ans.append(curr.val)
+                    break
+                else:
+                    curr = curr.children[ch]
+                    if i == 0:
+                        ans.append(curr.val)
+        return ans
