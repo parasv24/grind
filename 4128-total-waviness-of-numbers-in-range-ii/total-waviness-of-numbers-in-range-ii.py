413c85748121dbbd3526c373ca448bac5d7f8582
Paras Verma <90893097+parasv24@users.noreply.github.com>
2026-06-05 09:30:37 +0530

    Time: 798 ms (25.00%) | Memory: 63.9 MB (60.00%) - LeetSync

--- 4128-total-waviness-of-numbers-in-range-ii/total-waviness-of-numbers-in-range-ii.py
+++ 4128-total-waviness-of-numbers-in-range-ii/total-waviness-of-numbers-in-range-ii.py
@@ -1,35 +1,42 @@
 class Solution:
     def totalWaviness(self, num1: int, num2: int) -> int:
-        def get_count(s):
-            s = str(s)
+        def get_count(num):
+            s = str(num)
             @cache
-            def p_and_v(i, limit_on, prev2, prev1, started):
+            def rec(i, limit_on, prev2, prev1):
                 if i == len(s):
                     return (1, 0)
                 
                 limit = int(s[i]) if limit_on else 9
-                total_w = 0
-                ans = 0
+                ways, total = 0, 0
                 for j in range(0, limit + 1):
-                    new_started = started or j != 0
-                    if not new_started:
-                        total, small_ans = p_and_v(i+1, limit_on and j == limit, -1, -1, False)
-                        ans += small_ans
-                        total_w += total
+                    new_limit_on = limit_on and j == limit
+                    if j == 0 and prev2 == -1:
+                        x, y = rec(i+1, new_limit_on, -1, -1)
+                        total += x
+                        ways += y
+                        continue
+                    
+                    if prev2 == -1:
+                        next_prev2 = j
+                        next_prev1 = -1
+                    elif prev1 == -1:
+                        next_prev2 = prev2
+                        next_prev1 = j
                     else:
-                        peak = 0
-                        valley = 0
-
-                        # Only check after we have 3 digits
-                        if prev2 != -1 and prev1 != -1:
-                            peak = 1 if prev2 < prev1 > j else 0
-                            valley = 1 if prev2 > prev1 < j else 0
-                        cur = peak + valley
-                        total, small_ans = p_and_v(i+1, limit_on and j == limit, prev1, j, True)
-                        ans += small_ans + (cur * total)
-                        total_w += total
-                return (total_w, ans)
-            return p_and_v(0, True, -1, -1, False)[1]
+                        next_prev2 = prev1
+                        next_prev1 = j
+                    x, y = rec(i+1, new_limit_on, next_prev2, next_prev1)
+                    if prev2 != -1 and prev1 != -1:
+                        ways += (1 * x) if (prev2 < prev1 > j) or (prev2 > prev1 < j) else 0
+                    total += x
+                    ways += y
+                return (total, ways)
+            return rec(0, True, -1, -1)[1]
         return get_count(num2) - get_count(num1-1)
+
+
+
+
         
 
