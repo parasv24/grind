fc1645f604a433d57a248d4c98259ac42d90012d
Paras Verma <90893097+parasv24@users.noreply.github.com>
2026-06-06 12:21:20 +0530

    Time: 259 ms (80.07%) | Memory: 84.8 MB (88.68%) - LeetSync

--- 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.py
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.py
@@ -1,21 +1,28 @@
 class Solution:
     def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
-        gs = {}
-        for i in range(n):
-            gs[i]= []
-        for x,y in edges:
-            gs[x].append(y)
-            gs[y].append(x)
-        vis = [False] * n
-        vis[source] = True
-        queue = [source]
-        print(queue)
-        while len(queue) > 0:
-            node = queue[0]
-            queue.pop(0)
-            for n in gs[node]:
-                if not vis[n]:
-                    vis[n] = True
-                    queue.append(n)
-        return vis[destination]
+        parent = [i for i in range(n)]
+        size = [1] * n
+
+        def find(x):
+            if parent[x] != x:
+                parent[x] = find(parent[x])
+            return parent[x]
         
+        def union(x, y):
+            px = find(x)
+            py = find(y)
+
+            if px == py:
+                return False
+            
+            if size[px] < size[py]:
+                py, px = px, py
+            
+            parent[py] = px
+            size[px] += size[py]
+            return True
+
+        for x, y in edges:
+            union(x, y)
+        
+        return find(source) == find(destination)
