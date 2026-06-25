5f216fc460120d41f15ea0d85d9345d2e53b1c95
Paras Verma <90893097+parasv24@users.noreply.github.com>
2026-06-25 13:32:15 +0530

    Time: 1135 ms (27.52%) | Memory: 49.6 MB (5.50%) - LeetSync

--- 4075-count-subarrays-with-majority-element-ii/count-subarrays-with-majority-element-ii.py
+++ 4075-count-subarrays-with-majority-element-ii/count-subarrays-with-majority-element-ii.py
@@ -1,56 +1,37 @@
 class Solution:
     def countMajoritySubarrays(self, nums, target):
-        n = len(nums)
-        size = 2 * n + 1
-        offset = n
-
-        seg = [0] * (4 * size)
-
-        def update(node, l, r, pos):
+        pre = [0]
+        cur = 0
+        for el in nums:
+            x = 1 if el == target else -1
+            cur += x
+            pre.append(cur)
+        
+        def merge_sort(l, r):
             if l == r:
-                seg[node] += 1
-                return
-
+                return [pre[l]], 0
             mid = (l + r) // 2
+            left_arr, left_ans = merge_sort(l , mid)
+            right_arr, right_ans = merge_sort(mid+1, r)
 
-            if pos <= mid:
-                update(node * 2 + 1, l, mid, pos)
-            else:
-                update(node * 2 + 2, mid + 1, r, pos)
-
-            seg[node] = seg[node * 2 + 1] + seg[node * 2 + 2]
-
-        def query(node, l, r, ql, qr):
-            if qr < l or r < ql:
-                return 0
-
-            if ql <= l and r <= qr:
-                return seg[node]
-
-            mid = (l + r) // 2
-
-            return (
-                query(node * 2 + 1, l, mid, ql, qr)
-                + query(node * 2 + 2, mid + 1, r, ql, qr)
-            )
-
-        prefix = 0
-        ans = 0
-
-        # insert prefix[0] = 0
-        update(0, 0, size - 1, offset)
-
-        for x in nums:
-            if x == target:
-                prefix += 1
-            else:
-                prefix -= 1
-
-            pos = prefix + offset
-
-            if pos > 0:
-                ans += query(0, 0, size - 1, 0, pos - 1)
-
-            update(0, 0, size - 1, pos)
-
+            i = 0
+            count = left_ans + right_ans
+            for x in right_arr:
+                while i < len(left_arr) and left_arr[i] < x:
+                    i += 1
+                count += i
+            
+            i , j = 0, 0
+            combined_arr = []
+            while i < len(left_arr) and j < len(right_arr):
+                if left_arr[i] <= right_arr[j]:
+                    combined_arr.append(left_arr[i])
+                    i += 1
+                else:
+                    combined_arr.append(right_arr[j])
+                    j += 1
+            combined_arr.extend(left_arr[i:])
+            combined_arr.extend(right_arr[j:])
+            return combined_arr, count
+        _, ans = merge_sort(0, len(pre)-1)
         return ans
