f207313862492714e3ff63425f08516820d09736
Paras Verma <90893097+parasv24@users.noreply.github.com>
2026-07-02 12:09:22 +0530

    Time: 53 ms (91.04%) | Memory: 19.5 MB (97.17%) - LeetSync

--- 3558-find-a-safe-walk-through-a-grid/find-a-safe-walk-through-a-grid.py
+++ 3558-find-a-safe-walk-through-a-grid/find-a-safe-walk-through-a-grid.py
@@ -1,28 +1,21 @@
 class Solution:
     def findSafeWalk(self, grid: List[List[int]], health: int) -> bool:
-        z = 0
-        if grid[0][0] == 1:
-            z = 1
-        queue = deque([[0,0,z]])
-        n,m = len(grid), len(grid[0])
-        delta = [[-1, 0], [1, 0], [0, 1], [0, -1]]
-        grid[0][0] = -1
+        queue = deque([(0, 0, grid[0][0])])
+        grid[0][0] = 2
+        delta = [(-1, 0), (1, 0), (0, 1), (0, -1)]
         while queue:
-            x, y, z = queue.popleft()
-            # print(x,y,z)
-            if x == n - 1 and y == m - 1:
-                if z < health:
-                    return True
-                else:
-                    return False 
-            for i , j in delta:
-                xi, yi = x + i , y + j
-                if 0 <= xi <= n-1 and 0 <= yi <= m-1 and grid[xi][yi] != -1:
-                    if grid[xi][yi] == 0:
-                        queue.appendleft([xi, yi,z])
-                    else:
-                        queue.append([xi, yi, z+1])
-                    grid[xi][yi] = -1
-        return False
+            i, j, unsafe_count = queue.popleft()
 
-        
+            if i == len(grid) - 1 and j == len(grid[0]) - 1:
+                # print(unsafe_count, grid[-1][-1], health)
+                return unsafe_count < health
+            
+            for di , dj in delta:
+                ni, nj = i + di, j + dj
+                if 0 <= ni < len(grid) and 0 <= nj < len(grid[0]) and grid[ni][nj] != 2:
+                    if grid[ni][nj] == 0:
+                        queue.appendleft((ni, nj, unsafe_count))
+                    else:
+                        queue.append((ni, nj, unsafe_count + 1))
+                    grid[ni][nj] = 2
+            
